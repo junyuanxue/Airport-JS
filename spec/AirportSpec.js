3b1cc7d888975ef3d20a07a3dd98aabe30dc4e71
Junyuan Xue <junyuanxue.is@gmail.com>
2016-03-28 19:20:59 +0100

    writing first tests for airport and plane

---
+++ spec/AirportSpec.js
@@ -0,0 +1,8 @@
+"use strict";
+
+describe("Airport", function() {
+  var airport
+
+  beforeEach(function() {
+    airport = new Airport();
+  });
---
+++ spec/FeatureSpec.js
@@ -0,0 +1,17 @@
+"use strict";
+
+describe("FeatureTest:", function() {
+  var plane;
+  var airport;
+
+  beforeEach(function() {
+    plane = new Plane();
+    airport = new Airport();
+  });
+
+  it("planes can be instructed to land at the airport", function() {
+    plane.land(airport)
+    expect(airport.planes()).toContain(plane);
+  });
+
+});
