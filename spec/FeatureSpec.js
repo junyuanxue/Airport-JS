7da6517692c0741b99fa50488b8ba6a056c43338
Junyuan Xue <junyuanxue.is@gmail.com>
2016-03-28 21:06:40 +0100

    added landing and takeoff functions

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -14,4 +14,26 @@ describe("FeatureTest:", function() {
     expect(airport.planes()).toContain(plane);
   });
 
+  it("planes can be instructed to take off", function() {
+    plane.land(airport);
+    plane.takeoff();
+    expect(airport.planes()).not.toContain(plane);
+  });
+
+  it("blocks takeoff when weather is stormy", function() {
+    plane.land(airport)
+    spyOn(airport, "isStormy").and.returnValue(true);
+    expect(function() { plane.takeoff(); }).toThrowError("cannot takeoff during storm");
+    expect(airport.planes()).toContain(plane);
+  });
+
+  it("blocks landing when weather is stormy", function() {
+    plane.land(airport);
+    plane.takeoff();
+    spyOn(airport, "isStormy").and.returnValue(true);
+    expect(function() { plane.land(airport); }).toThrowError("cannot land during storm");
+    expect(airport.planes()).not.toContain(plane);
+  });
+
+
 });
--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -2,13 +2,22 @@
 
 describe("Plane", function() {
   var plane
+  var airport
 
   beforeEach(function() {
     plane = new Plane();
+    airport = jasmine.createSpyObj("airport", ["clearForLanding", "clearForTakeOff"]);
   });
 
   it("can land at an airport", function() {
-    expect(plane.land).not.toBeUndefined()
+    plane.land(airport);
+    expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
+  });
+
+  it("can take off from an airport", function() {
+    plane.land(airport);
+    plane.takeoff();
+    expect(airport.clearForTakeOff).toHaveBeenCalled();
   });
 
 });
